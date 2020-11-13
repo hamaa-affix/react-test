174ee574e7e45ab9f9ad9cc0def6dcd7c5668f64
hamaa-affix <59754313+hamaa-affix@users.noreply.github.com>
2020-11-13 22:52:43 +0900

    demo useState useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+/* eslint react-hooks/exhaustive-deps: off*/
+import React, { useEffect, useState } from "react";
 import ColofulMessage from "./components/ColofulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  // useState状態を定義　リアクティブデータを定義して、第二引数でデータを操作するメソッドを定義する。
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClicCountUp = () => {
+    setNum(num + 1);
   };
+  const onChengeFace = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   const contentStlye = {
     color: "red",
     fontsize: "18px"
@@ -23,7 +42,11 @@ const App = () => {
         </>
       </ColofulMessage>
       <ColofulMessage color="pink">test</ColofulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+      <button onClick={onClicCountUp}>カウントアップ</button>
+      <br />
+      {faceShowFlag && <p>😉</p>}
+      <button onClick={onChengeFace}>on/off</button>
     </>
   );
 };
